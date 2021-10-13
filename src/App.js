b062df7086dcdf93c49ff1769312c1d61d9dd0c3
JaiswalMudit <jaiswalmudit5@gmail.com>
2021-10-13 14:15:38 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -5,6 +5,39 @@ import { v4 as uuidv4 } from "uuid";
 export default function App() {
   return (
     <div className="App">
+      const [todos, settodo] = useState([]);
+  const todonameref = useRef();
+  const LOCAL_KEY = "aje";
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  useEffect(() => {
+    const storetodo = JSON.parse(localStorage.getItem(LOCAL_KEY));
+    if (storetodo) settodo(storetodo);
+  }, []);
+
+  function addtodo(e) {
+    const name = todonameref.current.value;
+    if (name === "") return;
+    settodo((prevtodo) => {
+      return [...prevtodo, { id: uuidv4(), name: name, completed: false }];
+    });
+    todonameref.current.value = null;
+  }
+
+  function toggletodo(id) {
+    const newtodo = [...todos];
+    const todo = newtodo.find((todo) => todo.id === id);
+    todo.completed = !todo.completed;
+    settodo(newtodo);
+  }
+
+  function cleartodo() {
+    const newtodo = todos.filter((todo) => !todo.completed);
+    settodo(newtodo);
+  }
       <h1>Hello CodeSandbox</h1>
       <Todolist todo={todos} toggle={toggletodo} />
       <input type="text" ref={todonameref} />
